5d04e5a0488f426b530106396e03c28defbda4fb
Nicolas Graf <nicolas.j.graf@gmail.com>
2023-05-20 03:24:09 +0200

    Change execution flow, remove input from master.
    Signed-off-by: Nicolas Graf <nicolas.j.graf@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,11 +11,10 @@ resourcedir="${basedir}/resource"
 autoresourcedir="${basedir}/autoresource"
 vardir="${basedir}/var"
 
-mkdir ${vardir}
 touch ${vardir}/input.txt
 
-if [ "$MASTERSERVER" = true ]; then
-    set -- "/armagetron/bin/armagetronad-master" \
+if [ -z ${MASTERSERVER+x} ]; then
+    set -- "${bindir}/armagetronad-dedicated" \
     --datadir ${datadir} \
     --userdatadir ${userdatadir} \
     --configdir ${configdir} \
@@ -25,17 +24,14 @@ if [ "$MASTERSERVER" = true ]; then
     --vardir ${vardir} \
     --input ${vardir}/input.txt
 else
-    set -- "/armagetron/bin/armagetronad-dedicated" \
+    set -- "${bindir}/armagetronad-master" \
     --datadir ${datadir} \
     --userdatadir ${userdatadir} \
     --configdir ${configdir} \
     --userconfigdir ${userconfigdir} \
     --resourcedir ${resourcedir} \
     --autoresourcedir ${autoresourcedir} \
-    --vardir ${vardir} \
-    --input ${vardir}/input.txt
+    --vardir ${vardir}
 fi
 
 exec "$@"
-
-/armagetron/bin/armagetronad-dedicated --datadir /armagetron/data --configdir /armagetron/config
